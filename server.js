a03d5965dcbbc9544f68b2f9117b9f074175be1f
DevA3SmartHome <krishna.komatlapalli@winwire.com>
2021-07-02 14:21:18 +0530

    commit

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ var headers = {
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
